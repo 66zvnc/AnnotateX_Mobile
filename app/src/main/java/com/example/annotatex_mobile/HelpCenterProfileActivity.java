9f4b87d555fae278b8d4d2f8f715df9bc6dcc732
Ivaylo Nedev <ivakkkk@icloud.com>
2024-11-24 19:58:26 +0200

    24.11

--- app/src/main/java/com/example/annotatex_mobile/HelpCenterProfileActivity.java
+++ app/src/main/java/com/example/annotatex_mobile/HelpCenterProfileActivity.java
@@ -2,7 +2,6 @@ package com.example.annotatex_mobile;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
@@ -13,7 +12,7 @@ public class HelpCenterProfileActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_profile_help_center);
+        setContentView(R.layout.activity_profile_help_center);
 
         // Set up the "Go Back" button functionality
         ImageView goBackButton = findViewById(R.id.goBackButton);
--- app/src/main/java/com/example/annotatex_mobile/PaymentMethodsProfileActivity.java
+++ app/src/main/java/com/example/annotatex_mobile/PaymentMethodsProfileActivity.java
@@ -2,7 +2,6 @@ package com.example.annotatex_mobile;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
@@ -13,7 +12,7 @@ public class PaymentMethodsProfileActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_profile_payment_methods);
+        setContentView(R.layout.activity_profile_payment_methods);
 
         // Set up the go-back button
         ImageView goBackButton = findViewById(R.id.goBackButton);
